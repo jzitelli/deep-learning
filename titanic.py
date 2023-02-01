e9a8202db9ec644bac33f832a91b6c67d8110097
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2023-01-31 19:58:27 -0500

    kaggle titanic competition

--- titanic.py
+++ titanic.py
@@ -11,15 +11,18 @@ try:
 except:
     data_dir = 'titanic'
 train_data = pd.read_csv(os.path.join(data_dir, 'train.csv'))
+test_data = pd.read_csv(os.path.join(data_dir, 'test.csv'))
 
 def extract_features(data):
-    columns = ['Pclass', 'SibSp', 'Parch', 'Fare']
-    return np.vstack([data[col].to_numpy('float32') for col in columns] +
-                     [(train_data['Sex'] == 'male').to_numpy('float32')]).T.copy()
+    columns = ['Pclass', 'SibSp', 'Parch']#, 'Fare']
+    result = np.vstack([data[col].to_numpy('float32') for col in columns] +
+                       [(data['Sex'] == 'male').to_numpy('float32'),
+                        pd.Categorical(data.Embarked).codes]).T.copy()
+    result -= result.mean(axis=0)
+    result /= result.std(axis=0)
+    return result
 
 td = extract_features(train_data)
-td -= td.mean(axis=0)
-td /= td.std(axis=0)
 
 model = keras.Sequential([
     layers.Dense(32, activation='relu'),
@@ -31,7 +34,7 @@ model = keras.Sequential([
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
 history = model.fit(td, train_data['Survived'].to_numpy('float32'),
-                    epochs=100, batch_size=64, validation_split=0.4)
+                    epochs=20, batch_size=64, validation_split=0.4)
 
 plt.figure()
 plt.plot(history.history['loss'], label='training loss')
@@ -42,3 +45,8 @@ plt.figure()
 plt.plot(history.history['accuracy'], label='training accuracy')
 plt.plot(history.history['val_accuracy'], label='validation accuracy')
 plt.legend()
+
+td = extract_features(test_data)
+
+predictions = model.predict(td)
+print(predictions)
