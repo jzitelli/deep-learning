8be16b76409d76b429d45a05819cf21c8875d845
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2023-02-10 21:58:42 -0500

    deep learning chapter 8 computer vision dogs n cats

--- deep_learning_chapter8.py
+++ deep_learning_chapter8.py
@@ -2,7 +2,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 import matplotlib.pyplot as plt
-# import numpy as np
+import numpy as np
 import os, shutil, pathlib
 
 
@@ -87,20 +87,75 @@ test_dataset = image_dataset_from_directory(new_base_dir / 'test',
                                              batch_size=32)
 
 # callbacks = [keras.callbacks.ModelCheckpoint(filepath='convnet_from_scratch.keras', save_best_only=True, monitor='val_loss')]
-callbacks = [keras.callbacks.ModelCheckpoint(filepath='convnet_from_scratch_with_augmentation.keras', save_best_only=True, monitor='val_loss')]
-history = model.fit(train_dataset, epochs=100, validation_data=validation_dataset, callbacks=callbacks)
+# callbacks = [keras.callbacks.ModelCheckpoint(filepath='convnet_from_scratch_with_augmentation.keras', save_best_only=True, monitor='val_loss')]
+# history = model.fit(train_dataset, epochs=100, validation_data=validation_dataset, callbacks=callbacks)
+#
+# plt.figure()
+# plt.plot(history.history['accuracy'], label='training accuracy')
+# plt.plot(history.history['val_accuracy'], label='validation accuracy')
+# plt.legend()
+#
+# plt.figure()
+# plt.plot(history.history['loss'][1:], label='training loss')
+# plt.plot(history.history['val_loss'][1:], label='validation loss')
+# plt.legend()
 
-plt.figure()
-plt.plot(history.history['accuracy'], label='training accuracy')
-plt.plot(history.history['val_accuracy'], label='validation accuracy')
-plt.legend()
+# # test_model = keras.models.load_model('convnet_from_scratch.keras')
+# test_model = keras.models.load_model('convnet_from_scratch_with_augmentation.keras')
+# test_model = keras.models.load_model('fine_tuning.keras')
+# test_loss, test_acc = model.evaluate(test_dataset)
+# print(f"test accuracy: {test_acc:.3f}")
 
-plt.figure()
-plt.plot(history.history['loss'][1:], label='training loss')
-plt.plot(history.history['val_loss'][1:], label='validation loss')
-plt.legend()
+conv_base = keras.applications.vgg16.VGG16(weights='imagenet', include_top=False, input_shape=(180,180,3))
 
-# test_model = keras.models.load_model('convnet_from_scratch.keras')
-test_model = keras.models.load_model('convnet_from_scratch_with_augmentation.keras')
-test_loss, test_acc = model.evaluate(test_dataset)
-print(f"test accuracy: {test_acc:.3f}")
+def get_features_and_labels(dataset):
+    all_features = []
+    all_labels = []
+    for images, labels in dataset:
+        preprocessed_images = keras.applications.vgg16.preprocess_input(images)
+        features = conv_base.predict(preprocessed_images)
+        all_features.append(features)
+        all_labels.append(labels)
+    return np.concatenate(all_features), np.concatenate(all_labels)
+
+train_features, train_labels = get_features_and_labels(train_dataset)
+val_features, val_labels = get_features_and_labels(validation_dataset)
+test_features, test_labels = get_features_and_labels(test_dataset)
+
+inputs = keras.Input(shape=(5,5,512))
+x = layers.Flatten()(inputs)
+x = layers.Dense(256)(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1, activation='sigmoid')(x)
+model = keras.Model(inputs, outputs)
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+callbacks = [keras.callbacks.ModelCheckpoint(filepath='feature_extraction.keras', save_best_only=True, monitor='val_loss')]
+history = model.fit(train_features, train_labels, epochs=20,
+                    validation_data=(val_features, val_labels), callbacks=callbacks)
+
+conv_base.trainable = False
+inputs = keras.Input(shape=(180,180,3))
+x = data_augmentation(inputs)
+x = keras.applications.vgg16.preprocess_input(x)
+x = conv_base(x)
+x = layers.Flatten()(x)
+x = layers.Dense(256)(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1, activation='sigmoid')(x)
+model = keras.Model(inputs, outputs)
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+callbacks = [keras.callbacks.ModelCheckpoint(filepath='feature_extraction_with_data_augmentation.keras',
+                                             save_best_only=True, monitor='val_loss')]
+history = model.fit(train_dataset, epochs=50,
+                    validation_data=validation_dataset, callbacks=callbacks)
+
+conv_base.trainable = True
+for layer in conv_base.layers[:-4]:
+    layer.trainable = False
+model.compile(loss='binary_crossentropy', optimizer=keras.optimizers.RMSprop(learning_rate=1e-5), metrics=['accuracy'])
+callbacks = [keras.callbacks.ModelCheckpoint(filepath='fine_tuning.keras',
+                                             save_best_only=True, monitor='val_loss')]
+history = model.fit(train_dataset, epochs=30,
+                    validation_data=validation_dataset, callbacks=callbacks)
