fix visualizing softmax output

deep_learning_chapter9.py
@@ -80,8 +80,9 @@ def compare_target(model, i):
     plt.figure(); plt.title('image')
     plt.imshow(val_input_imgs[i] / 255.0)
     plt.figure(); plt.title('predicted')
-    #plt.imshow(model.predict(np.expand_dims(val_input_imgs[i], axis=0))[0] * 2.0)
-    plt.imshow(model.predict(val_input_imgs[i].reshape((1,) + img_size + (3,)))[0] * 2.0)
+    pred = model.predict(val_input_imgs[i:i + 1])[0]
+    mask = np.argmax(pred, axis=-1)
+    plt.imshow(mask * 127)
     plt.figure(); plt.title('actual')
     plt.imshow(val_targets[i] / 255.0)
 